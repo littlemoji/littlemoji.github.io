a43dff9e784aa3814ddea417ff7b2c51ad31d5bc
littlemoji <littlemoji@foxmail.com>
2021-06-27 23:40:45 +0800

    feat:初创

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 编译生成静态文件
+yarn run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 发布到上面建立的第一个仓库
+git push -f git@github.com:USERNAME/USERNAME.github.io.git master
+
+# 返回上一次工作目录
+cd -
